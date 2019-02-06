2a79cd2c737162b2c2de72795f8018a0b94ba1c0
Unknown <tiagoveronesi@gmail.com>
2019-02-05 22:55:31 -0200

    Preparando o Banco de Dados
    Migrate ratings, irá estabelecer o relacionamento polimorfico entre Model Posts e Users, será possível realizar uma avaliação de um post e tambem de um usuario

---
+++ database/migrations/2019_02_06_004150_create_ratings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRatingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('message',200);
+            $table->unsignedInteger('ratingable_id');
+            $table->char('ratingable_type',200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ratings');
+    }
+}
