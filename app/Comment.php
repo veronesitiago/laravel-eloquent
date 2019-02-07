8471288180249c213efd9222c8c121897da3cfd7
tiago <tiagoveronesi@outcenter.com.br>
2019-02-07 19:10:11 -0200

    Usando mutador

--- app/Comment.php
+++ app/Comment.php
@@ -17,4 +17,14 @@ class Comment extends Model
    {
      return $this->belongsTo('App\Post', 'post_id', 'id');
    }
+
+   /**
+    * Define titulo com primeira letra maiuscula
+    * O mutador deve começar com set + nome_campo/coluna + Attribute
+    * @return void
+    */
+   public function setTitleAttribute($value)
+   {
+      $this->attributes['title'] = ucfirst($value);
+   }
 }
